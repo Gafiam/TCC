8d22d4402a5fa7887f67bc989ed843377d2bc75d
Gabriel S Medeiros <gabrielzsm@gmail.com>
2022-07-19 20:55:40 -0300

    att_final

--- iJIM/iJIM/spiders/papers.py
+++ iJIM/iJIM/spiders/papers.py
@@ -1,5 +1,4 @@
 import scrapy
-
 class PapersSpider(scrapy.Spider):
     name = 'papers'
     allowed_domains = ['online-journals.org']
@@ -10,8 +9,8 @@ class PapersSpider(scrapy.Spider):
         for journal in journals:
             vol_num = journal.xpath(".//text()").get()
             link = journal.xpath(".//@href").get()
-
-            yield response.follow(url=link, callback=self.parse_journal, meta={'journal_name': vol_num})
+            yield response.follow(url=link, callback=self.parse_journal, 
+                meta={'journal_name': vol_num})
 
     def parse_journal(self, response):
         name = response.request.meta['journal_name']
@@ -19,16 +18,18 @@ class PapersSpider(scrapy.Spider):
         for article in articles:
             title = article.xpath(".//div/h3/a/text()").get()
             link_article = article.xpath(".//div/h3/a/@href").get()
-
-            yield response.follow(url=link_article, callback=self.parse_paper, meta={'article_name': title})
+            yield response.follow(url=link_article, callback=self.parse_paper, 
+                meta={'article_name': title})
 
     def parse_paper(self, response):
         paper = response.request.meta['article_name']
-        keywords = response.xpath("//section[@class='item keywords']/span[@class='value']/text()").re('.*')
+        keywords = response.xpath(
+            "//section[@class='item keywords']/span[@class='value']/text()").re('.*')
         published = response.xpath("//div[@class='value']/span/text()").re('.*')
         abstract = response.xpath("//section[@class='item abstract']/p/text()").re('.*')
         abstract2 = response.xpath("//section[@class='item abstract']/text()").re('.*')
-        abstract3 = response.xpath("//section[@class='item abstract']/br/text()").re('.*')
+        abstract3 = response.xpath(
+            "//section[@class='item abstract']/br/text()").re('.*')
         yield {
             'paper_name': paper,
             'keywords_name': keywords,
